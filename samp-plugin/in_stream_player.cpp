047dfb76003baab56d97b11f5cc14f5f48e74c59
jostkiy waparabka <waparabka@gmail.com>
2024-01-03 06:25:15 +0600

    better detection when player should be removed added blip markers removing

--- samp-plugin/in_stream_player.cpp
+++ samp-plugin/in_stream_player.cpp
@@ -7,18 +7,21 @@ bool InStreamPlayer::is_contains(uint16_t player_id) {
 }
 
 
-void InStreamPlayer::add(world_player_add_s player) {
+void InStreamPlayer::add(world_player_add_s world_player) {
 
-	if (!is_contains(player.player_id)) {
+	if (!is_contains(world_player.player_id)) {
+        
+		players_iterator.insert(world_player.player_id);
+		players.push_back(world_player);
 
-		players_iterator.insert(player.player_id);
-		players.push_back(player);
+        if (!should_be_deleted(world_player.player_id))
+            create(world_player);
 	}
 }
 
 
 void InStreamPlayer::remove(uint16_t player_id) {
-
+	
 	if (is_contains(player_id)) {
 
 		players_iterator.erase(player_id);
@@ -26,7 +29,144 @@ void InStreamPlayer::remove(uint16_t player_id) {
 		for (auto it = players.begin(); it != players.end(); it++)
 			if (it->player_id == player_id) {
 				players.erase(it); break; }
+
+        auto player = samp::RefNetGame()->GetPlayerPool()->GetPlayer(player_id);
+
+        if (!player)
+            return;
+
+        player->SetMarkerState(0);
 	}
+}
+
+
+void InStreamPlayer::create(world_player_add_s world_player) {
+    
+    RakNet::BitStream bs { };
+    
+    bs.Write((char*)&world_player, sizeof(world_player_add_s));
+
+    rakhook::emul_rpc(32, bs);
+}
+
+
+bool InStreamPlayer::should_be_deleted(uint16_t player_id) { // TODO : add skin's condition
+
+    auto player = samp::RefNetGame()->GetPlayerPool()->GetPlayer(player_id);
+
+    std::stringstream player_color;
+    player_color << std::hex << std::uppercase << player->GetColorAsARGB();
+
+
+    for (const auto& [key, val] : config->config["config"]["fractions"].items()) {
+        
+        for (const auto& [key_2, val_2] : val.items()) {
+            
+            if (config->config["config"]["fractions"]["all"]["state"].get<bool>())
+                return true;
+
+            
+            if (config->config["config"]["misc"]["not_delete_incar_players"]["state"].get<bool>()) {
+
+                auto vehicle = player->m_pVehicle;
+
+                if (vehicle && vehicle->DoesExist())
+                    return false;
+            }
+
+            
+            if (config->config["config"]["misc"]["not_delete_bobcat_players"]["state"].get<bool>()) {
+
+                bool is_bobcat = false;
+
+                for (auto const& [key_3, val_3] : config->config["bobcats"].items()) {
+
+                    auto vehicle = player->m_pVehicle;
+
+                    if (!vehicle || !vehicle->DoesExist())
+                        break;
+
+                    if (vehicle->GetModelIndex() == val_3) {
+                        is_bobcat = true; break; }
+                }
+
+                if (is_bobcat)
+                    return false;
+            }
+
+
+            if (config->config["config"]["misc"]["enable_friend_list"]["state"].get<bool>()) {
+
+                std::string name = samp::RefNetGame()->GetPlayerPool()->GetName(player_id);
+
+                bool is_friend = false;
+
+                for (auto const& [key_3, val_3] : config->config["friends"].items())
+                    if (key_3 == name) {
+                        is_friend = true; break; }
+
+                if (is_friend)
+                    return false;
+            }
+            
+
+            std::string config_color = config->config["fractions"][key]["color"].get<std::string>();
+
+            if (val_2 && player_color.str() == config_color)
+                return true;
+        }
+    }
+    
+    return false;
+}
+
+
+void InStreamPlayer::process() {
+
+    for (auto it = begin(); it != end(); it++) {
+
+        auto player = samp::RefNetGame()->GetPlayerPool()->GetPlayer(*it);
+
+        if (!player)
+            continue;
+
+        bool should_be_deleted = this->should_be_deleted(*it);
+        
+        if (should_be_deleted && player->DoesExist()) {
+            player->Remove();
+        }
+        else if (config->toggled && !player->DoesExist()) {
+            create(get_updated(*it, player->m_onfootData.m_position));
+        }
+        
+        
+        if (config->config["config"]["misc"]["not_delete_markers"]["state"].get<bool>()) {
+            
+            if (player->DoesExist())
+                continue;
+
+            auto position = player->m_onfootData.m_position;
+            
+            if (position.GetLength() == 0) {
+
+                auto world_player = get(*it);
+                auto world_player_position = sampapi::CVector(world_player.x, world_player.y, world_player.z);
+                
+                if (std::isinf(world_player_position.GetLength()))
+                    continue;
+
+                position = world_player_position;
+            }
+            
+            player->SetMarkerPosition(position.x, position.y, position.z);
+        }
+        else if (config->toggled) {
+            player->SetMarkerState(0);
+        }
+    }
+    
+    if (config->toggled)
+        config->toggled = false;
 }
 
 
@@ -50,6 +190,16 @@ InStreamPlayer::world_player_add_s InStreamPlayer::get_updated(uint16_t player_i
 	}
 	
 	return updated;
+}
+
+
+InStreamPlayer::world_player_add_s InStreamPlayer::get(uint16_t player_id) {
+    
+    for (auto it = players.begin(); it != players.end(); it++)
+        if (it->player_id == player_id)
+            return *it;
+    
+    return { 0 };
 }
 
 
--- samp-plugin/in_stream_player.h
+++ samp-plugin/in_stream_player.h
@@ -4,6 +4,9 @@
 #include <set>
 #include <vector>
 #include <sampapi/CVector.h>
+#include <RakNet/BitStream.h>
+#include <RakHook/rakhook.hpp>
+#include "config.h"
 
 
 class InStreamPlayer {
@@ -20,9 +23,13 @@ public:
 	};
 
 	bool is_contains(uint16_t player_id);
-	void add(world_player_add_s player);
+	void add(world_player_add_s world_player);
 	void remove(uint16_t player_id);
+	void create(world_player_add_s world_player);
+	bool should_be_deleted(uint16_t player_id);
+	void process();
 	world_player_add_s get_updated(uint16_t player_id, sampapi::CVector position);
+	world_player_add_s get(uint16_t player_id);
 
 	std::set<uint16_t>::iterator begin();
 	std::set<uint16_t>::iterator end();
@@ -30,4 +37,6 @@ public:
 private:
 	std::vector<world_player_add_s> players;
 	std::set<uint16_t> players_iterator;
+
+	Config* config = Config::Get();
 };
