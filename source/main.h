baa990027df7e25fb86769c2720588a08c2e8e25
waparabka <68443529+waparabka@users.noreply.github.com>
2024-06-03 16:50:18 +0600

    cosmetic

--- source/main.h
+++ source/main.h
@@ -15,10 +15,6 @@ public:
 	c_entry();
 	~c_entry();
 
-	//std::unique_ptr<cyanide::polyhook_x86<destroy_ri_t, decltype(&destroy_rakclient_interface)>> destroy_ri_hook;
-	
-	std::unique_ptr<kthook::kthook_simple<prototype::void_t>> test_hook { };
-
 	kthook::kthook_simple<prototype::void_t> ctimer_hook { reinterpret_cast<void*>(0x561B10) };
 	void update(const decltype(ctimer_hook)& hook);
 
